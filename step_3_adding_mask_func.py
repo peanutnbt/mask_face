cdc56ced81e9526805389a9a9c50b3d98b08a6ef
peanutnbt <nguyenbaotan125590@gmail.com>
2021-03-29 11:01:31 +0700

    edit not write box and lanmark file

--- step_3_adding_mask_func.py
+++ step_3_adding_mask_func.py
@@ -18,7 +18,7 @@ def add_mask(image, image_path, mask_image_file, first_dir, align_val):
     #
     # face_lms_str = open(face_lms_file).readline().strip().split(' ')
     face_lms_str = align_val.split(' ')
-    print("----------------face_lms_str: ", face_lms_str)
+    # print("----------------face_lms_str: ", face_lms_str)
     face_lms = [float(num) for num in face_lms_str]
     face_masker = FaceMasker(is_aug)
     face_masker.add_mask_one(image_path, face_lms, template_name, masked_face_path, image)
